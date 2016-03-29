c95943a2ba4e6702261615beb770a4867938645e
yale918 <yale918@gmail.com>
2016-03-29 13:51:25 +0800

    ..

--- lang/javascript/testCode/test.js
+++
@@ -1,9 +0,0 @@
-//<script>
-/*
-  <button type="button" onclick="document.getElementById('demo').innerHTML=Date()">Click me to show Time.</button>
- <p id="demo"></p>
-*/
-function myFunc(){
-	alert("hello world");
-}
-// </script>
