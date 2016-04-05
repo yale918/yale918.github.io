603c42c6d0252aa35d30a816da68d3bf149c583c
yale918 <yale918@gmail.com>
2016-04-05 22:06:14 +0800

    ..

---
+++ linux/kernel-modules/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/yale918/coding/yale918.github.io/linux/kernel-modules/hello.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/yale918/coding/yale918.github.io/linux/kernel-modules/hello.ko /home/yale918/coding/yale918.github.io/linux/kernel-modules/hello.o /home/yale918/coding/yale918.github.io/linux/kernel-modules/hello.mod.o
---
+++ linux/kernel-modules/.tmp_versions/hello.mod
@@ -0,0 +1,2 @@
+/home/yale918/coding/yale918.github.io/linux/kernel-modules/hello.ko
+/home/yale918/coding/yale918.github.io/linux/kernel-modules/hello.o
