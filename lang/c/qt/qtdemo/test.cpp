51bee5b3b7c0d6f6b91841523d259627ed5b6525
Yale <yale918@gmail.com>
2016-06-26 14:15:14 +0800

    ..

---
+++ lang/c/qt/qtdemo/test.cpp
@@ -0,0 +1,10 @@
+#include<QTGui/QApplication>
+#include<QTGui/QPushButton>
+
+int main(int argc, char *argv[]){
+    QApplication application(argc, argv);
+    QPushButton button("Hello World");
+    button.show();
+    return application.exec();
+
+}
